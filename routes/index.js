7f3c65648499fd6b41201e6cfe515fd8c900d63f
Xunto <hunterdelyx1@gmail.com>
2016-06-26 23:23:44 +0300

    Basic routes

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/:shortUrl', function (request, response) {
+    response.redirect(303, 'http://test.com');
+});
+
+router.post('/shorten', function (request, response) {
+    response.send(request.get('host') + '/' + to_url(0));
+});
+
 module.exports = router;
